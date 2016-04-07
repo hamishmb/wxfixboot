87bd9b147a971f53c403f6756d54c3a966235bc0
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2016-04-07 16:48:58 +0100

    Disgnostics in CoreStartupTools.CheckForGRUBBIOS()

--- Tools/StartupTools/core.py
+++ Tools/StartupTools/core.py
@@ -110,6 +110,7 @@ class Main():
     def CheckForGRUBBIOS(self, MBR):
         """Check for the GRUB (v2 and legacy) BIOS bootloader"""
         #*** Try to find a way of distinguishing between them here, rather than later *** *** Test again ***
+        print(str(MBR))
         if str("GRUB") in str(MBR): #*** Avoid UnicodeDecodeError ***
             #Bootloader is GRUB MBR
             return True
