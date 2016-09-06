229a8dcac7d05e7f202e37b2fe95ea612af2cdfd
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2016-09-06 10:34:26 +0100

    Diagnostics

--- Tools/StartupTools/getbootloaderconfigtools.py
+++ Tools/StartupTools/getbootloaderconfigtools.py
@@ -621,8 +621,8 @@ class Main(): #*** Refactor all of these ***
 
         #Find the 1st menu entry and use that if we couldn't find the default OS.
         if DefaultOS == "Unknown":
-            for Entry in MenuEntries["MainMenu"]:
-                if MenuEntries["MainMenu"][Entry]["ID"] == 0:
+            for Entry in BootloaderInfo["OS"]["MenuEntries"]["MainMenu"]:
+                if BootloaderInfo["OS"]["MenuEntries"]["MainMenu"][Entry]["ID"] == 0:
                     DefaultOS = Entry
                     logger.info("BootloaderConfigObtainingTools: Main().GetGRUB2Config(): Set default OS to "+Entry+" instead. Continuing...")
 
