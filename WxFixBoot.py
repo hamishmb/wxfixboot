46236c50e60d2572de177eab50d22f1f8cf3b6d7
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2016-09-12 12:15:28 +0100

    Fixed Progress Bar weirdness when doing more than one operation at once

--- WxFixBoot.py
+++ WxFixBoot.py
@@ -19,7 +19,6 @@
 #*** Is /etc/default/grub created after switching to grub if it was purged before? ***
 #*** Test disabling bootloader operations, as if a filesystem check failed ***
 #*** Elilo not available in Ubuntu 16.04 + ***
-#*** Progress bars look a little glitchy when doing more than 1 bootloader operation ***
 
 #Do future imports to prepare to support python 3. Use unicode strings rather than ASCII strings, as they fix potential problems.
 from __future__ import absolute_import
