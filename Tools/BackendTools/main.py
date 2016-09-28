0765cecbad284f83dccdee0818d011e2af1af058
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2016-09-28 10:14:13 +0100

    Make sure GRUB2 is installed to the right disk.

--- Tools/BackendTools/main.py
+++ Tools/BackendTools/main.py
@@ -442,7 +442,7 @@ class Main():
             else:
                 #Now Install GRUB2 to the MBR.
                 logger.info("MainBackendTools: Main().SetNewBootloaderConfig(): Installing GRUB2 to "+DiskInfo[OSInfo[OS]["Partition"]]["HostDevice"]+"...")
-                BootloaderConfigSettingTools.InstallGRUB2ToMBR(PackageManager=OSInfo[OS]["PackageManager"], UseChroot=UseChroot, MountPoint=MountPoint, Device=DiskInfo[OSInfo[OS]["Partition"]]["HostDevice"])
+                BootloaderConfigSettingTools.InstallGRUB2ToMBR(PackageManager=OSInfo[OS]["PackageManager"], UseChroot=UseChroot, MountPoint=MountPoint, Device=BootloaderInfo[OS]["BootDisk"])
 
             #Update GRUB.
             logger.info("MainBackendTools: Main().SetNewBootloaderConfig(): Updating GRUB2 Configuration...")
