19c7266c55c86f7368e7ce0cd4a5c5cec7d62121
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-09-07 15:50:11 +0100

    Finish the refactoring

---
+++ Tools/dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Dictionaries in the Tools Package for WxFixBoot Version 3.0.0
+# This file is part of WxFixBoot.
+# Copyright (C) 2013-2018 Hamish McIntyre-Bhatty
+# WxFixBoot is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 or,
+# at your option, any later version.
+#
+# WxFixBoot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WxFixBoot.  If not, see <http://www.gnu.org/licenses/>.
+if not 'SYSTEM_INFO' in globals():
+    SYSTEM_INFO = {}
+
+if not 'DISK_INFO' in globals():
+    DISK_INFO = {}
+
+if not 'OS_INFO' in globals():
+    OS_INFO = {}
+
+if not 'BOOTLOADER_INFO' in globals():
+    BOOTLOADER_INFO = {}
+
+if not 'SETTINGS' in globals():
+    SETTINGS = {}
