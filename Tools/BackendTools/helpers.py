a838f795ded6220acc3d1a08423a34b2d37e25c9
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-10-23 12:57:08 +0100

    Attempt to fix issue when there's no DNF cache

--- Tools/BackendTools/helpers.py
+++ Tools/BackendTools/helpers.py
@@ -108,7 +108,7 @@ def wait_until_packagemanager_free(mount_point, package_manager):
         retval = CoreTools.start_process(cmd, show_output=False)
 
         #Get the package cache if there is none. 200 - locking failure.
-        if package_manager == "yum" and retval != 200:
+        if package_manager == "yum" and retval not in (0, 200):
             CoreTools.start_process("sh -c 'echo No cache available, downloading package information...'")
             CoreTools.start_process("yum check-update", show_output=False)
 
