dbd01a1d27fe464b57d223d018f8c4f276ee4d28
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2019-03-25 14:44:48 +0000

    When writing privileged files, stop trimming leading whitespace

--- Tools/helpers/runasroot_linux_write_file.sh
+++ Tools/helpers/runasroot_linux_write_file.sh
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with WxFixBoot.  If not, see <http://www.gnu.org/licenses/>.
 
+#Clear the IFS variable to prevent read from stripping leading whitespace.
+IFS=
+
 #Read the first line of input.
 first=true
 read -r line < /dev/stdin
