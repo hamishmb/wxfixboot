ea233c13d9d2f9e20079fd44478536373d3e98e8
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2019-04-17 17:03:18 +0100

    Handle error codes 126 and 127 is a more robust way

--- Tools/coretools.py
+++ Tools/coretools.py
@@ -122,8 +122,8 @@ def start_process(exec_cmds, show_output=True, return_output=False, testing=Fals
                  +": Return Value: "+unicode(ret_val)
                  + ", Output: \"\n\n"+'\n'.join(line_list)+"\"\n")
 
-    #This causes issues with chroot - 127 is returned - causing infinite recursion.
-    if privileged and (ret_val == 126 or ret_val == 127) and "chroot" not in exec_cmds:
+    #Handle these error codes if pkexec is being used.
+    if privileged and (ret_val in (126, 127)) and "pkexec" in exec_cmds:
         #Try again, auth dismissed / bad password 3 times.
         #A lot of recursion is allowed (~1000 times), so this shouldn't be a problem.
         logger.debug("start_process(): Bad auth or dismissed by user. Trying again...")
