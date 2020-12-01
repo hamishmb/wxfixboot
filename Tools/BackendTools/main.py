3c0de981b01bda32f088497cbe4097f5aa5af025
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2020-12-01 16:48:46 +0000

    Add '--allow-remove-essential' - required for removing bootloader pkgs on Ubuntu 20.10

--- Tools/BackendTools/main.py
+++ Tools/BackendTools/main.py
@@ -287,7 +287,7 @@ def remove_old_bootloader(_os):
 
         if OS_INFO[_os]["PackageManager"] == "apt-get":
             cmd = "sh -c 'DEBIAN_FRONTEND=gnome DISPLAY=:0 apt-get purge -y " \
-                  "grub-pc grub-pc-bin grub-common'"
+                  "--allow-remove-essential grub-pc grub-pc-bin grub-common'"
 
         elif OS_INFO[_os]["PackageManager"] == "dnf":
             cmd = "dnf -y remove grub2"
@@ -297,8 +297,8 @@ def remove_old_bootloader(_os):
 
         if OS_INFO[_os]["PackageManager"] == "apt-get":
             cmd = "sh -c 'DEBIAN_FRONTEND=gnome DISPLAY=:0 apt-get purge -y " \
-                  "grub-efi grub-efi-amd64 grub-efi-amd64-bin grub-efi-ia32 " \
-                  "grub-efi-ia32-bin grub-common grub2-common'"
+                  "--allow-remove-essential grub-efi grub-efi-amd64 grub-efi-amd64-bin" \
+                  "grub-efi-ia32  grub-efi-ia32-bin grub-common grub2-common'"
 
         elif OS_INFO[_os]["PackageManager"] == "dnf":
             cmd = "dnf -y remove grub2-efi-x64 grub2-efi-ia32 shim-x64"
